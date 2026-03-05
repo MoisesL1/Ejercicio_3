94a439fd878076dee7408960529f3977395d78d6
Moises <moiseslopez1155@gmail.com>
2026-03-04 19:24:02 -0600

    Ingreso de datos.

--- Program.cs
+++ Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿Dictionary <string, double> notas = new Dictionary<string, double>();
+
+Console.WriteLine("Ingrese la cantidad de estudiates que ingresara: ");
+int cant = int.Parse(Console.ReadLine());
+
+string estudiante;
+
+for(int x = 0; x < cant; x++)
+{
+    Console.WriteLine("Ingrese el nombre del estudiante: ");
+    estudiante = Console.ReadLine();
+    Console.WriteLine("Ingrese la nota: ");
+    double nota = double.Parse(Console.ReadLine());
+
+    notas.Add(estudiante, nota);
+}
